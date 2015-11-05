a3479b04916766c2c31b5f95d3e1cf43a15db173
Virussteyn <viru.steyn@gmail.com>
2015-11-05 11:44:40 +0800

    ok

--- game/game.js
+++ game/game.js
@@ -4,5 +4,8 @@ $( document ).ready(function() {
 	$("#playBtn").click(function() {
 		$("#Gamebody").css("background-image", "url('https://upload.wikimedia.org/wikipedia/en/c/c3/Wwtbam-uk-2010.png')");
 		$("#playBtn").hide();
-	});
+	}); //ask for your name
+	var name = prompt("What is your name?");
+	// The question you are given has to be answered correctly but if answered wrong the game will stop
+	var 
 });
