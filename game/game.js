d4b8865161e9f3055e78837ab77a689580208717
Virussteyn <viru.steyn@gmail.com>
2015-11-05 14:53:18 +0800

    ok

--- game/game.js
+++ game/game.js
@@ -4,5 +4,16 @@ $( document ).ready(function() {
 	$("#playBtn").click(function() {
 		$("#Gamebody").css("background-image", "url('https://upload.wikimedia.org/wikipedia/en/c/c3/Wwtbam-uk-2010.png')");
 		$("#playBtn").hide();
+		//asks for your name
+		var name = prompt("What is your name?")
+		// the cash they have in the beginning and how many lifelines they have in the beginning
+		var moneyearned = o;
+		var lifeline = 3;
+
+		function lifelineCheck() { 
+        // this function checks the lifelines and and if they have 0 then it will take them to the end screen!
+        if (lifeline===0) {
+
+		}
     }); 
 });
