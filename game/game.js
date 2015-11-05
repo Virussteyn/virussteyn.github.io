6b60cae1166dc739aadd749478571515d03651fa
Virussteyn <viru.steyn@gmail.com>
2015-11-05 15:16:34 +0800

    k bruh

--- game/game.js
+++ game/game.js
@@ -1,19 +1,30 @@
 //this is my js file//
-$( document ).ready(function() {
+$(document).ready(function() {
 
 	$("#playBtn").click(function() {
 		$("#Gamebody").css("background-image", "url('https://upload.wikimedia.org/wikipedia/en/c/c3/Wwtbam-uk-2010.png')");
 		$("#playBtn").hide();
-		//asks for your name
-		var name = prompt("What is your name?")
-		// the cash they have in the beginning and how many lifelines they have in the beginning
-		var moneyearned = o;
-		var lifeline = 3;
 
-		function lifelineCheck() { 
-        // this function checks the lifelines and and if they have 0 then it will take them to the end screen!
-        if (lifeline===0) {
-
-		}
     }); 
 });
+
+var begin = prompt("are you ready");
+var correctAnswer = 0;
+while (correctAnswer!=1) {
+var question1 = prompt("What is the correct answer? A, B, or C?").toUpperCase();
+
+switch(question1) {
+    case 'A':
+    alert("wrong");
+    break;
+        
+    case 'B':
+    alert("correct");
+    correctAnswer = 1;
+    break;
+        
+    case 'C':
+    alert("wrong, try again");
+    break;
+}
+}
