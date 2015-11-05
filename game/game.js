c940eb375726d730fd5827505e4e5fd66c714ba3
Virussteyn <viru.steyn@gmail.com>
2015-11-05 11:46:53 +0800

    ok

--- game/game.js
+++ game/game.js
@@ -4,7 +4,5 @@ $( document ).ready(function() {
 	$("#playBtn").click(function() {
 		$("#Gamebody").css("background-image", "url('https://upload.wikimedia.org/wikipedia/en/c/c3/Wwtbam-uk-2010.png')");
 		$("#playBtn").hide();
-	}); //ask for your name
-	var name = prompt("What is your name?");
     }); 
 });
